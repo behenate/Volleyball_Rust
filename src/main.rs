66e998a033217533fe56628ff3e68d5bf6b7f130
Mateusz4444z <81083639+Mateusz4444z@users.noreply.github.com>
2021-11-13 21:27:11 +0100

    boisk + siatka

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,28 @@ fn main()-> tetra::Result {
 struct GameState {
     player1: Entity,
     player2: Entity,
+    court: Entity,
+    voley: Entity,
 }
 impl GameState{
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
         let player1_texture = Texture::new(ctx, "./resources/player1.png")?;
         let player2_texture = Texture::new(ctx, "./resources/player2.png")?;
+        let court_texture = Texture::new(ctx, "./resources/court.jpg")?;
+        let voley_texture = Texture::new(ctx, "./resources/voley.png")?;
         let player1_position =
-            Vec2::new(16.0, (WINDOW_HEIGHT - player1_texture.height() as f32) / 2.0);
+            Vec2::new(16.0, WINDOW_HEIGHT - player1_texture.height() as f32 -court_texture.height() as f32);
         let player2_position =
-            Vec2::new(300.0, (WINDOW_HEIGHT - player2_texture.height() as f32) / 2.0);
+            Vec2::new(WINDOW_WIDTH - 16.0 - player1_texture.width() as f32, WINDOW_HEIGHT - player2_texture.height() as f32 -court_texture.height() as f32);
+        let court_position =
+            Vec2::new(0.0, WINDOW_HEIGHT - court_texture.height() as f32);
+        let voley_position =
+            Vec2::new(320.0-voley_texture.width() as f32/2.0, WINDOW_HEIGHT - court_texture.height() as f32 - voley_texture.height() as f32);
         Ok(GameState { 
             player1: Entity::new(player1_texture, player1_position), 
-            player2: Entity::new(player2_texture, player2_position)
+            player2: Entity::new(player2_texture, player2_position),
+            court: Entity::new(court_texture, court_position),
+            voley: Entity::new(voley_texture, voley_position),
          })
     }
     
@@ -35,9 +45,12 @@ impl GameState{
 impl State for GameState{
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
     
         self.player1.texture.draw(ctx, self.player1.position);
         self.player2.texture.draw(ctx, self.player2.position);
+        self.court.texture.draw(ctx, self.court.position);
+        self.voley.texture.draw(ctx, self.voley.position);
         Ok(())
     }
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
